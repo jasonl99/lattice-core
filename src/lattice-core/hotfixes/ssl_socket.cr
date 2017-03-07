31f3f76fb10f27bdf7671c2d846de741495ede35
Jason Landry <jasonl99@gmail.com>
2017-03-07 11:50:36 -0500

    wip

--- src/lattice-core/hotfixes/ssl_socket.cr
+++ src/lattice-core/hotfixes/ssl_socket.cr
@@ -1,12 +1,12 @@
-abstract class OpenSSL::SSL::Socket
-  class Server < Socket
-    def initialize(io, context : Context::Server = Context::Server.new, sync_close : Bool = false)
-      super(io, context, sync_close)
+# abstract class OpenSSL::SSL::Socket
+#   class Server < Socket
+#     def initialize(io, context : Context::Server = Context::Server.new, sync_close : Bool = false)
+#       super(io, context, sync_close)
 
-      ret = LibSSL.ssl_accept(@ssl)
-      unless ret == 1
-        io.close if sync_close   # this is the hotfix
-        raise OpenSSL::SSL::Error.new(@ssl, ret, "SSL_accept")
-      end
-    end
-end
+#       ret = LibSSL.ssl_accept(@ssl)
+#       unless ret == 1
+#         io.close if sync_close   # this is the hotfix
+#         raise OpenSSL::SSL::Error.new(@ssl, ret, "SSL_accept")
+#       end
+#     end
+# end
